8fb8f38ff6103fd531a99c78a64eb5eb91aa4ba8
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2022-10-28 08:45:57 +0600

    data added

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const cors = require("cors");
 app.use(cors());
 
 const Port = process.env.Port || 5000;
+
+const courseCollection = require("./Data/data.json");
+
+app.get("/", (req, res) => {
+  res.send("server test ");
+});
+
+app.get("/courseCollection", (req, res) => {
+  res.send(courseCollection);
+});
+
+app.listen(Port, () => {
+  console.log("server is running ", Port);
+});
